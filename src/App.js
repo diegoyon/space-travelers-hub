bd3ac97030801a690649b1e4f8fbfbde23d23bdc
Diego Yon <diegoyon@outlook.com>
2022-08-22 15:55:15 -0600

    Add route to profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" exact element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import planet from '../images/planet.png';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <>
       <img src={planet} alt="planet" />
+      <NavLink
+        to='/'
+        className={({ isActive }) => (
+          isActive ? 'active' : null
+        )}
+      >
+        Profile
+      </NavLink>
     </>
   )
 }
