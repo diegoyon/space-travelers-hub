089da275e0d7980e1fcab4001b75282e90c7b77d
Diego Yon <diegoyon@outlook.com>
2022-08-22 16:08:16 -0600

    Add rockets page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from "./pages/Profile";
+import Rockets from "./pages/Rockets";
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Profile />} />
+        <Route path="/" exact element={<Rockets />} />
+        <Route path="/profile" exact element={<Profile />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,27 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <>
+    <nav>
       <img src={planet} alt="planet" />
-      <NavLink
-        to='/'
-        className={({ isActive }) => (
-          isActive ? 'active' : null
-        )}
-      >
-        Profile
-      </NavLink>
-    </>
+      <div className='links'>
+        <NavLink
+          to='/'
+          className={({ isActive }) => (
+            isActive ? 'active' : null
+          )}
+        >
+          Rockets
+        </NavLink>
+        <NavLink
+          to='/profile'
+          className={({ isActive }) => (
+            isActive ? 'active' : null
+          )}
+        >
+          Profile
+        </NavLink>
+      </div>
+    </nav>
   )
 }
 
