68896e2d04ac91eb260088cbc5bca8b6456a5e6d
Diego Yon <diegoyon@outlook.com>
2022-08-22 16:48:37 -0600

    Fetch rockets from API

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
 
 function Rockets() {
+  const dispatch = useDispatch();
+  const rocketList = useSelector((state)=> state.rockets.rockets)
+  const rocketStatus = useSelector((state) => state.rockets.status)
+
+  useEffect(() => {
+    if (rocketStatus === 'idle') {
+      dispatch(fetchRockets());
+    }
+  }, [rocketStatus, dispatch]);
+  
+  console.log(rocketList);
+
   return (
     <div>Rockets</div>
   )
---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const ROCKETS_URL = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk(
+  'users/fetchRockets',
+  async () => {
+    try {
+      const response = await axios.get(ROCKETS_URL);
+      const rocketsObject = response.data.map((rocket) => {
+        const { id, rocket_name, rocket_type, flickr_images } = rocket;
+        return { id, rocket_name, rocket_type, flickr_images };
+      });
+      return rocketsObject;
+    } catch (err) {
+      return err.message;
+    }
+  },
+);
+
+const initialState = {
+  rockets: [],
+  status: 'idle',
+  error: null,
+};
+
+export const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRockets.fulfilled, (state,action) =>{
+        state.status = 'succeeded';
+        state.rockets = action.payload;
+      })
+  }
+});
+
+export default rocketsSlice.reducer;
