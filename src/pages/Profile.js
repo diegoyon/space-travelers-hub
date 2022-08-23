79615ef70ab44bd33cde23bc123e62bd0da5560b
Diego Yon <diegoyon@outlook.com>
2022-08-23 09:57:58 -0600

    Display rockets on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './Profile.css';
 
 function Profile() {
+  const rocketList = useSelector((state) => state.rockets.rockets);
+  const filteredList = rocketList.filter((rocket) => (rocket.reserved === true));
   return (
-    <div>Profile</div>
+    <>
+      <div className="profile">
+        <div className="my-missions">
+          <h2>My Missions</h2>
+          {/* pending */}
+        </div>
+        <div className="my-rockets">
+          <h2>My Rockets</h2>
+          {filteredList.map((rocket) => (
+            <p key={rocket.id}>{rocket.rocket_name}</p>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
