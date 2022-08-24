6b2e01b50f5925bb531ce4ca4f7591b10566825b
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-08-25 01:46:12 +0500

    Update files

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import MissionLi from '../components/Missions';
+import { getMissionsDataAPI } from '../redux/missions/missions';
+import '../components/missions.css';
 
-function Missions() {
-  return (
-    <div>Missions</div>
+const Missions = () => {
+  const data = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+  useEffect(
+    () => {
+      if (data.length === 0) dispatch(getMissionsDataAPI());
+    },
+    [],
   );
-}
+  return (
+    <ul className="missions-container">
+      <li className="mission-li">
+        <p className="mission-name">Mission</p>
+        <p className="mission-description">Description</p>
+        <p className="mission-status">Status</p>
+        <div className="mission-status"> Action </div>
+      </li>
+      {data.map((mission) => (
+        <MissionLi
+          key={mission.mission_id}
+          id={mission.mission_id}
+          name={mission.mission_name}
+          description={mission.description}
+          reserved={mission.reserved}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default Missions;
